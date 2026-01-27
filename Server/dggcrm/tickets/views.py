3b6d387a073252b9a0d6a58b58ded10a5e62ae4b
sgainso <jptjasc@gmail.com>
2026-01-27 17:00:00 -0500

    #59 making sure everything is up

--- Server/dggcrm/tickets/views.py
+++ Server/dggcrm/tickets/views.py
@@ -33,10 +33,11 @@ class TicketViewSet(viewsets.ModelViewSet):
         event_id = self.request.query_params.get('event')
         contact_id = self.request.query_params.get('contact')
 
+        exclude_status = self.request.query_params.get('exclude_status')
+
         if ticket_status is not None:
             queryset = queryset.filter(ticket_status=ticket_status)
 
-        exclude_status = self.request.query_params.get('exclude_status')
         if exclude_status is not None:
             queryset = queryset.exclude(ticket_status__in=exclude_status.split(','))
 
